4c9a8bcd678d042317a350cf50a1615c9440fa29
mudox <imudox@gmail.com>
2017-12-01 00:26:10 +0800

    Update pod paths in `Example/Podfile`

--- Example/Pods/Target Support Files/Pods-iOSKit_Example/Pods-iOSKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-iOSKit_Example/Pods-iOSKit_Example-frameworks.sh
@@ -102,9 +102,13 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/JacKit/JacKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/iOSKit/iOSKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/JacKit/JacKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/iOSKit/iOSKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then

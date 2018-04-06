23e29aa4e3e474577853d48e5bafcdc4dfed9db6
mudox <imudox@gmail.com>
2018-04-07 04:27:03 +0800

    Improve MainVC, remove more VC from storyboard

--- Example/iOSKit/MainVC.swift
+++ Example/iOSKit/MainVC.swift
@@ -22,15 +22,35 @@ class MainVC: FormViewController {
 
     tableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
 
-    form +++ Section("View Controllers")
+    form +++ Section("View Controller Extensions")
+
     <<< ButtonRow() {
-      $0.title = "Alert & Action Sheet Layout API"
-      $0.presentationMode = .segueName(segueName: "AlertVC", onDismiss: nil)
+      $0.title = "UIAlertController Layout String"
+      $0.presentationMode = .show(controllerProvider: .callback(builder: AlertVC.init), onDismiss: nil)
     }
+
     <<< ButtonRow() {
-      $0.title = "Interactive Navigation Conroller Pop"
+      $0.title = "Interactive Pop"
       $0.presentationMode = .show(controllerProvider: .callback(builder: NavPopVC.init), onDismiss: nil)
     }
+
+    form +++ Section("HUD controls")
+
+    <<< ButtonRow() {
+      $0.title = "MBProgressHUD"
+      $0.presentationMode = .show(controllerProvider: .callback(builder: MbpVC.init), onDismiss: nil)
+    }
+
+    <<< ButtonRow() {
+      $0.title = "SVProgressHUD"
+      $0.presentationMode = .show(controllerProvider: .callback(builder: SvpVC.init), onDismiss: nil)
+    }
+
+    <<< ButtonRow() {
+      $0.title = "NVActivityIndicatorView"
+      $0.presentationMode = .show(controllerProvider: .callback(builder: NvaVC.init), onDismiss: nil)
+    }
+
   }
 
 }

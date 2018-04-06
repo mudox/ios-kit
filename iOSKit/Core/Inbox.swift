b78237425bf8d1b871094c222b93b0846ccea382
mudox <imudox@gmail.com>
2018-04-07 04:25:11 +0800

    Add overloads of method `with`

--- iOSKit/Core/Inbox.swift
+++ iOSKit/Core/Inbox.swift
@@ -13,3 +13,19 @@ extension Bool {
     self = !self
   }
 }
+
+public func with<T>(_ target: T, apply change: (T) -> ()) {
+  change(target)
+}
+
+public func with<T>(_ target: T, apply change: (T) throws -> ()) rethrows {
+  try change(target)
+}
+
+public func with<T, Result>(_ target: T, evaluate: (T) -> Result) -> Result {
+  return evaluate(target)
+}
+
+public func with<T, Result>(_ target: T, tryEvaluate evaluate: (T) throws -> Result) rethrows -> Result {
+  return try evaluate(target)
+}

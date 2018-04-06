6e6b5c57571cbf90ef66fee9a325470539ebd83d
mudox <imudox@gmail.com>
2018-04-07 04:24:50 +0800

    Add protocol `HasEnumNames`

---
+++ iOSKit/Core/HasEnumNames.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  iOSKit
+//
+//  Created by Mudox on 2018/4/7.
+//
+
+import UIKit
+
+protocol HasEnumNames {
+  
+  static var enumName: String { get }
+  var fullName: String { get }
+  var caseName: String { get }
+  
+}
+
+extension HasEnumNames {
+  
+  public var fullName: String {
+    return "\(Self.enumName).\(caseName)"
+  }
+  
+}
+
+extension UIAlertControllerStyle: HasEnumNames {
+  
+  public static let enumName = "UIAlertControllerStyle"
+
+  public var caseName: String {
+    switch self {
+    case .actionSheet: return "actionSheet"
+    case .alert: return "alert"
+    }
+  }
+
+}

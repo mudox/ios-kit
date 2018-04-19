e0d73f0464397f0dab3dd18f223e579f3aaf0d16
mudox <imudox@gmail.com>
2018-04-20 02:46:38 +0800

    Migrate tests into Quick specs

--- Example/Tests/IntSpec.swift
+++ Example/Tests/IntSpec.swift
@@ -9,19 +9,21 @@ class IntSpec: QuickSpec {
 
     describe("Int") {
 
-      it("can express time units") {
-        expect(1.minuteInSecond) == 60
-        expect(1.hourInSecond) == 60 * 60
-        expect(1.dayInSecond) == 60 * 60 * 24
-        expect(1.weekInSecond) == 60 * 60 * 24 * 7
+      it("minute hour day week") {
+        expect(1.minute) == 60
+        expect(1.hour) == 60 * 60
+        expect(1.day) == 60 * 60 * 24
+        expect(1.week) == 60 * 60 * 24 * 7
       }
 
-      it("can expresss bytes size") {
-        expect(1.KB) == 1024
-        expect(1.MB) == 1024 * 1024
-        expect(1.GB) == 1024 * 1024 * 1024
-        expect(1.TB) == 1024 * 1024 * 1024 * 1024
+      it("kb mb gb tb") {
+        expect(1.kb) == 1024
+        expect(1.mb) == 1024 * 1024
+        expect(1.gb) == 1024 * 1024 * 1024
+        expect(1.tb) == 1024 * 1024 * 1024 * 1024
       }
-    }
+      
+    } // describe("Int")
+    
   }
 }

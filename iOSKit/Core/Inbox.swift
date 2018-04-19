1b6739bda47e6393d3f69b20dade0bf7b2247476
mudox <imudox@gmail.com>
2018-04-20 01:56:48 +0800

    Add `Collection` extension methods: `forEachConcurrently`, `subscript(safe:)`

--- iOSKit/Core/Inbox.swift
+++ iOSKit/Core/Inbox.swift
@@ -1,31 +1,7 @@
-//
-//  Inbox.swift
-//  Pods
-//
-//  Created by Mudox on 28/03/2017.
-//
-//
-
 import Foundation
 
 extension Bool {
   public mutating func toggle() {
     self = !self
   }
-}
-
-public func with<T>(_ target: T, apply change: (T) -> ()) {
-  change(target)
-}
-
-public func with<T>(_ target: T, apply change: (T) throws -> ()) rethrows {
-  try change(target)
-}
-
-public func with<T, Result>(_ target: T, evaluate: (T) -> Result) -> Result {
-  return evaluate(target)
-}
-
-public func with<T, Result>(_ target: T, tryEvaluate evaluate: (T) throws -> Result) rethrows -> Result {
-  return try evaluate(target)
 }
